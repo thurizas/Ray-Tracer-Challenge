ef5c46bb0b954f92fa41200f999d4222e1ed074f
thurizas <thurizas123@gmail.com>
2020-07-19 14:12:23 -0400

    Add files via upload

---
+++ chapter02/point.h
@@ -0,0 +1,38 @@
+#pragma once
+
+class vector;
+
+class point
+{
+public:
+    point();
+    point(float x, float y, float z, float w = 1);
+    point(const point& rhs);                       // copy constucture
+    point& operator=(const point& rhs);            // assignment operator
+
+    bool isPoint() { return m_w == 1; }
+    bool isVector() { return m_w != 1; }
+
+    const float  x() { return m_x; }
+    void x(float v) { m_x = v; }
+    const float y() { return m_y; }
+    void y(float v) {m_y = v;}
+    const float z() { return m_z; }
+    void z(float v) { m_z = v; }
+    const float w() { return m_w; }
+
+    bool operator==(const point& rhs);
+    point operator-();                   //uniary negation operator
+    point operator+(const vector& rhs);
+    point operator-(const vector& rhs);
+    vector operator-(const point& rhs);
+
+
+private:
+    float m_x;
+    float m_y;
+    float m_z;
+    float m_w;   // must be 1 for a point 
+
+
+};
